d997109c503b3e705f71f247f0a9637dc99ffeab
Khant Thu <bienew311@gmail.com>
2022-01-17 14:12:41 +0630

    active state

---
+++ script.js
@@ -0,0 +1,8 @@
+const custom_button = document.querySelector(".custom");
+const custom_input = document.querySelector(".custom_input");
+
+function custominput(){
+    custom_button.classList.add("custom_hidden");
+
+    custom_input.classList.add("custom_input_show");
+}
